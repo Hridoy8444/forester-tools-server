7a88b2f3148f7b73487174932be0291951286e7d
fardous hridoy <mdreday736@gmail.com>
2022-05-28 00:31:26 +0600

    error fix

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,18 +8,35 @@ const port = process.env.PORT || 5000;
 
 //use middleware
 app.use(cors());
-app.use(express.json);
+app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dpa8t.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t3wydbn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('db connected');
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        const toolCollection = client.db('manufacturer-website').collection('tools');
+        console.log('connected');
+
+        app.get('/tool', async (req, res) => {
+            const query = {}
+            const cursor = toolCollection.find(query);
+            const tools = await cursor.toArray();
+            res.send(tools);
+        });
+    }
+    finally {
+
+    }
+}
+
+
+
+
+run().catch(console.dir);
 
 
 //db user1
